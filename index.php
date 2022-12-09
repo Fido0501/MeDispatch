3c1d2a511a2559eaeffba835c299ced2f3ba7ca9
Fido0501 <filip51.krivanek@gmail.com>
2022-12-10 00:23:11 +0100

    update

--- index.php
+++ index.php
@@ -23,48 +23,7 @@
 
         <!-- Main -->
         <section>
-            <div class="s1">
-                <div class="dev"></div>
-                <h1>WEBDESIGNER</h1>
-                <h1>WEBDEVELPOER</h1>
-                <div class="dev"></div>
-                <div class="scroll-container">
-                    <div class="scroll">Scroll down</div>
-                    <div class="scroll-icon"></div>
-                </div>
-            </div>
-        </section>
-
-        <section class="s2">
-            <div class="s2-container">
-                <div class="s2-content">
-                    <h6>About me</h6>
-                    <div class="dev2"></div>
-                    <div class="s2-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam ornare wisi eu metus. Etiam bibendum elit eget erat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </div>
-                    <div class="button-container">
-                        <div class="pri-btn">Projects</div>
-                        <div class="sec-btn">Contact me</div>
-                    </div>
-                </div>
-                <img class="s2-img" src="/img/picture.png" alt="">
-            </div>
-        </section>
-
-        <section>
-            <div class="s3">
-                <div class="s3-hd">SKILLZ</div>
-                <div class="dev3"></div>
-                <div class="s3-container">
-                    <div class="s3-column">
-                        <div class="item">cd</div>
-
-                    </div>
-                    <div class="s3-column">
-                        <div class="item">cd</div>
-                    </div>
-                </div>
-            </div>
+            
         </section>
 
         <!-- Footer -->
